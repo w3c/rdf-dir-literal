2a8220c552dfdc21c7885fe91c3e0f142585b471
Ivan Herman <ivan@ivan-herman.net>
2019-05-23 18:35:30 +0200

    Added a first draft for the introduction

---
+++ biblio.js
@@ -0,0 +1,50 @@
+/*
+ * WARNING: Use this file sparingly!
+ *
+ * Check http://www.specref.org/ first
+ *
+ */
+
+/* general entry template
+   "short_name": {
+      "title": "...",
+      "href": "https://...",
+      "authors": [
+        "..."
+      ],
+      "date": "..."
+   }
+*/
+
+var biblio = {
+    "wpub-ed": {
+        "title": "Web Publications",
+        "authors" : [
+            "Matt Garrish",
+            "Ivan Herman"
+        ],
+        "date": "2019-05-22",
+        "status": "W3C Editors' Draft",
+        "href": "https://w3c.github.io/wpub/",
+        "publisher": "W3C"
+    },
+    "inline-bid-markup" : {
+        "title" : "Inline markup and bidirectional text in HTML",
+        "authors" : [
+            "Richard Ishida",
+            "Aharon Lanin"
+        ],
+        "date": "2016-07-25",
+        "href" : "https://www.w3.org/International/articles/inline-bidi-markup/",
+        "publisher" : "W3C"
+    },
+    "uba-basics" : {
+        "title" : "Unicode Bidirectional Algorithm basics",
+        "authors" : [
+            "Richard Ishida"
+        ],
+        "date": "2016-08-09",
+        "href" : "https://www.w3.org/International/articles/uba-basics/",
+        "publisher" : "W3C"        
+    }
+}
